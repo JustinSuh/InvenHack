397b6736d09f149cd6b277efa3bf09a0f32a5332
Justin Suh <jsuh@smu.edu>
2016-04-16 22:10:20 -0500

    Update settings.php
    work please

--- settings.php
+++ settings.php
@@ -1,7 +1,4 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');  
-
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
@@ -17,12 +14,46 @@ return [
             'path' => __DIR__ . '/../logs/app.log',
         ],
 
+        'dbConn' => [
+            'username' => 'guest',
+            'password' => '',
+            'host' => 'localhost',
+            'dbname' => 'apiary',
+            'db' => 'mysql',
+        ],
+
+        'testImage' => [
+            'username' => 'guest',
+            'password' => '',
+            'host' => 'localhost',
+            'dbname' => 'MMTest',
+            'db' => 'mysql',
+        ],
+
+        'login_dbConn' => [ 
+            'username' => 'basic_user', 
+            'password' => 'abc123', 
+            'host' => 'localhost', 
+            'dbname' => 'test_auth', 
+            'db' => 'mysql', 
+        ],
+
         'user' => [
             'username' => 'root',
             'password' => '',
             'host' => 'localhost',
             'dbname' => 'inventory',
             'db' => 'mysql',
+        ],
+
+//***********************************************************************************
+
+        'api_login' => [ 
+            'username' => 'api_test',         
+            'password' => 'qwe123',              
+            'host' => 'localhost',              
+            'dbname' => 'pocketgains',          
+            'db' => 'mysql', 
         ],
     ],
 ];
